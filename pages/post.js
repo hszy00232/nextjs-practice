51f68f24fdd4131d99548415b407d5b8f969b64c
guopeipei <guopeipei@360.cn>
2021-05-07 17:50:40 +0800

    服务端切换成express

--- pages/post.js
+++ pages/post.js
@@ -4,7 +4,7 @@ import fetch from "isomorphic-unfetch";
 const Post = (props) => (
   <Layout>
     <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?p>/g, "")}</p>
+    <p>{props.show.summary.replace(/<[/]?[p|b]>/g, "")}</p>
     <img src={props.show.image.medium} alt={`${props.show.name}`} />
   </Layout>
 );
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const { createServer } = require("http");
-const { parse } = require("url");
+const express = require('express');
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
@@ -7,16 +6,15 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    const { pathname, query } = parsedUrl;
+  const server = express();
 
-    if (pathname === "/p") {
-      app.render(req, res, "/post", query);
-    } else {
-      handle(req, res, parsedUrl);
-    }
-  }).listen(3000, (err) => {
+  server.get('/p/:id',(req,res)=>{
+    app.render(req, res, "/post", {id:req.params.id});
+  })
+  server.get('*',(req,res) => {
+    handle(req,res);
+  })
+  server.listen(3000, (err) => {
     if (err) throw err;
     console.log("> Ready on http://localhost:3000");
   });
